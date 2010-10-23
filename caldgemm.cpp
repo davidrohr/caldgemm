3ab2827397378fb05a50d31679708a2a7701d114
David Rohr <drohr@jwdt.org>
2010-10-23 15:19:52 +0200

    added additional async timers

--- caldgemm.cpp
+++ caldgemm.cpp
@@ -1671,7 +1671,7 @@ int caldgemm::RunCALDGEMM(double* a, double* b, double* c, double alpha, double 
 						if (Info->AsyncTiming)
 						{
 							Timers.ATime.Stop();
-							fprintf(STD_OUT, "\t\tWait Time for merge thread: %1.5lf\n", Timers.ATime.GetElapsedTime());
+							fprintf(STD_OUT, "\t\tWait Time for output buffer: %1.5lf\n", Timers.ATime.GetElapsedTime());
 						}
 					}
 					WAITFOREVENT(ctx_main, j);
@@ -1726,7 +1726,17 @@ int caldgemm::RunCALDGEMM(double* a, double* b, double* c, double alpha, double 
 					}
 					else
 					{
+						if (Info->AsyncTiming)
+						{
+							Timers.ATime.Reset();
+							Timers.ATime.Start();
+						}
 						if (pthread_mutex_lock(&mParam[iMergeThread].mergeThreadMutex[1])) fprintf(STD_OUT, "Error locking mutex: %s - %d\n", __FILE__, __LINE__);
+						if (Info->AsyncTiming)
+						{
+							Timers.ATime.Stop();
+							fprintf(STD_OUT, "\t\tWait Time for merge thread: %1.5lf\n", Timers.ATime.GetElapsedTime());
+						}
 						if (Info->Debug) fprintf(STD_OUT, "\t\tUnlocking outputthread mutex %d to process context %d\n", iMergeThread, oldj);
 						mParam[iMergeThread].nContext = oldj;
 						mParam[iMergeThread].dst = C + (lastn * Info->Height + lastm * C_pitch * Info->Height);
