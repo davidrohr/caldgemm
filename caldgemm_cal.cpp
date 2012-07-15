53ae832c080e1d8019f321d2f454520df961e91a
David Rohr <drohr@jwdt.org>
2012-07-15 21:52:32 +0200

    try to simplify dividebuffer, not finished

--- caldgemm_cal.cpp
+++ caldgemm_cal.cpp
@@ -345,69 +345,35 @@ int caldgemm_cal::divideBuffer(BufferProperties* dst, double* src, int width, in
 #else
 		for (int i = 0;i < height;i += 2)
 		{
-			for (int y = 0;y < width;y += 16)
+			double* __restrict__ daddr0 = &dst[0].ptr_double[i * gpu_pitch];
+			double* __restrict__ daddr1 = &dst[1].ptr_double[i * gpu_pitch];
+
+			const double* __restrict__ saddr0 = &src[i];
+
+			for (int y = 0;y < width;y += 4)
 			{
-				const double* __restrict__ saddr0 = &src[(y + 0) * pitch + i];
-				const double* __restrict__ saddr2 = &src[(y + 2) * pitch + i];
-				const double* __restrict__ saddr4 = &src[(y + 4) * pitch + i];
-				const double* __restrict__ saddr6 = &src[(y + 6) * pitch + i];
 				
 #ifdef CALDGEMM_USE_VEC_MEMCPY_PREFETCH
-				_mm_prefetch(CAST_FOR_MMPREFETCH (saddr0 + 8 * pitch), _MM_HINT_NTA);
-				_mm_prefetch(CAST_FOR_MMPREFETCH (saddr2 + 8 * pitch), _MM_HINT_NTA);
-				_mm_prefetch(CAST_FOR_MMPREFETCH (saddr4 + 8 * pitch), _MM_HINT_NTA);
-				_mm_prefetch(CAST_FOR_MMPREFETCH (saddr6 + 8 * pitch), _MM_HINT_NTA);
-				_mm_prefetch(CAST_FOR_MMPREFETCH (saddr0 + 9 * pitch), _MM_HINT_NTA);
-				_mm_prefetch(CAST_FOR_MMPREFETCH (saddr2 + 9 * pitch), _MM_HINT_NTA);
-				_mm_prefetch(CAST_FOR_MMPREFETCH (saddr4 + 9 * pitch), _MM_HINT_NTA);
-				_mm_prefetch(CAST_FOR_MMPREFETCH (saddr6 + 9 * pitch), _MM_HINT_NTA);
+				_mm_prefetch(CAST_FOR_MMPREFETCH (saddr0 + 4 * pitch), _MM_HINT_T1);
+				_mm_prefetch(CAST_FOR_MMPREFETCH (saddr0 + 5 * pitch), _MM_HINT_T1);
+				_mm_prefetch(CAST_FOR_MMPREFETCH (saddr0 + 6 * pitch), _MM_HINT_T1);
+				_mm_prefetch(CAST_FOR_MMPREFETCH (saddr0 + 7 * pitch), _MM_HINT_T1);
 #endif
-
-				double* __restrict__ dstBank0 = &dst[0].ptr_double[y / 2];
-				double* __restrict__ dstBank1 = &dst[1].ptr_double[y / 2];
-				double* __restrict__ daddr0 = &dstBank0[i * gpu_pitch];
-				double* __restrict__ daddr1 = &dstBank1[i * gpu_pitch];
 				{
 					const __m128d x0 = _mm_load_pd_use(&saddr0[0]);
 					const __m128d x1 = _mm_load_pd_use(&saddr0[pitch]);
-					const __m128d x2 = _mm_load_pd_use(&saddr2[0]);
-					const __m128d x3 = _mm_load_pd_use(&saddr2[pitch]);
-					const __m128d x4 = _mm_load_pd_use(&saddr4[0]);
-					const __m128d x5 = _mm_load_pd_use(&saddr4[pitch]);
-					const __m128d x6 = _mm_load_pd_use(&saddr6[0]);
-					const __m128d x7 = _mm_load_pd_use(&saddr6[pitch]);
+					const __m128d x2 = _mm_load_pd_use(&saddr0[2 * pitch]);
+					const __m128d x3 = _mm_load_pd_use(&saddr0[3 * pitch]);
 
 					_mm_store_pd_use(&daddr0[0 + CALDGEMM_SHIFT_TEXTURE_OFFSET], _mm_unpacklo_pd(x0, x1));
 					_mm_store_pd_use(&daddr0[gpu_pitch], _mm_unpackhi_pd(x0, x1));
 					_mm_store_pd_use(&daddr1[0 + CALDGEMM_SHIFT_TEXTURE_OFFSET], _mm_unpacklo_pd(x2, x3));
 					_mm_store_pd_use(&daddr1[gpu_pitch], _mm_unpackhi_pd(x2, x3));
-
-					_mm_store_pd_use(&daddr0[2 + CALDGEMM_SHIFT_TEXTURE_OFFSET], _mm_unpacklo_pd(x4, x5));
-					_mm_store_pd_use(&daddr0[gpu_pitch + 2], _mm_unpackhi_pd(x4, x5));
-					_mm_store_pd_use(&daddr1[2 + CALDGEMM_SHIFT_TEXTURE_OFFSET], _mm_unpacklo_pd(x6, x7));
-					_mm_store_pd_use(&daddr1[gpu_pitch + 2], _mm_unpackhi_pd(x6, x7));
+					
 				}
-
-				{
-					const __m128d x0 = _mm_load_pd_use(&saddr0[8*pitch]);
-					const __m128d x1 = _mm_load_pd_use(&saddr0[9*pitch]);
-					const __m128d x2 = _mm_load_pd_use(&saddr2[8*pitch]);
-					const __m128d x3 = _mm_load_pd_use(&saddr2[9*pitch]);
-					const __m128d x4 = _mm_load_pd_use(&saddr4[8*pitch]);
-					const __m128d x5 = _mm_load_pd_use(&saddr4[9*pitch]);
-					const __m128d x6 = _mm_load_pd_use(&saddr6[8*pitch]);
-					const __m128d x7 = _mm_load_pd_use(&saddr6[9*pitch]);
-
-					_mm_store_pd_use(&daddr0[4 + CALDGEMM_SHIFT_TEXTURE_OFFSET], _mm_unpacklo_pd(x0, x1));
-					_mm_store_pd_use(&daddr0[gpu_pitch + 4], _mm_unpackhi_pd(x0, x1));
-					_mm_store_pd_use(&daddr1[4 + CALDGEMM_SHIFT_TEXTURE_OFFSET], _mm_unpacklo_pd(x2, x3));
-					_mm_store_pd_use(&daddr1[gpu_pitch + 4], _mm_unpackhi_pd(x2, x3));
-
-					_mm_store_pd_use(&daddr0[6 + CALDGEMM_SHIFT_TEXTURE_OFFSET], _mm_unpacklo_pd(x4, x5));
-					_mm_store_pd_use(&daddr0[gpu_pitch + 6], _mm_unpackhi_pd(x4, x5));
-					_mm_store_pd_use(&daddr1[6 + CALDGEMM_SHIFT_TEXTURE_OFFSET], _mm_unpacklo_pd(x6, x7));
-					_mm_store_pd_use(&daddr1[gpu_pitch + 6], _mm_unpackhi_pd(x6, x7));
-				}
+				daddr0 += 2;
+				daddr1 += 2;
+				saddr0 += 4 * pitch;
 			}
 		}
 #endif //CALDGEMM_SHIFT_TEXTURE
