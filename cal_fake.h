722f1cec02d6273cc6a8a8ed8f963a861dc83ec0
David Rohr <drohr@jwdt.org>
2012-10-25 21:00:12 +0200

    improve cal_fake header, implement through mode

--- cal_fake.h
+++ cal_fake.h
@@ -10,7 +10,9 @@
 #define NUM_FAKE_MEM 10000
 #define NUM_FAKE_MODULE 100
 #define NUM_FAKE_NAME 1000
-#define NUM_MODULE_NAMES 12
+#define NUM_MODULE_NAMES 24
+
+#define PASSTHROUGH
 
 class cal_fake_event
 {
@@ -22,6 +24,7 @@ public:
 	double delay;
 	int mems[NUM_MODULE_NAMES];
 	int nmems;
+	CALevent through;
 
 	cal_fake_event() {initialized = queried = reused = 0;}
 };
@@ -31,6 +34,8 @@ class cal_fake_mem
 public:
 	int released;
 	int active;
+	
+	CALmem through;
 };
 
 class cal_fake_module
@@ -39,12 +44,17 @@ public:
 	int released;
 	int nnames;
 	int names[NUM_MODULE_NAMES];
+	
+	CALmodule through;
+	CALfunc throughFunc;
 };
 
 class cal_fake_name
 {
 public:
 	int mem;
+	
+	CALname through;
 };
 
 class cal_fake
@@ -121,16 +131,18 @@ public:
 		else
 		{
 			event[num].timer.Stop();
-			if (event[num].timer.GetElapsedTime() > event[num].delay)
+#ifndef PASSTHROUGH
+			if (event[num].timer.GetElapsedTime() <= event[num].delay)
+			{
+				event[num].timer.Start();
+				retVal = CAL_RESULT_PENDING;
+			}
+			else
+#endif
 			{
 				event[num].queried = 1;
 				for (int i = 0;i < event[num].nmems;i++) mem[event[num].mems[i]].active = 0;
 				retVal = CAL_RESULT_OK;
-			}
-			else
-			{
-				event[num].timer.Start();
-				retVal = CAL_RESULT_PENDING;
 			}
 		}
 		pthread_mutex_unlock(&mutex);
@@ -144,7 +156,7 @@ public:
 		if (curmem == NUM_FAKE_MEM)
 		{
 			fprintf(stderr, "NUM_FAKE_MEM overflow\n");
-			exit(1);
+			while(true);
 		}
 		*m = curmem;
 		mem[curmem].released = 0;
@@ -160,7 +172,7 @@ public:
 		if (curmodule == NUM_FAKE_MODULE)
 		{
 			fprintf(stderr, "NUM_FAKE_MODULE overflow\n");
-			exit(1);
+			while(true);
 		}
 		*mod = curmodule;
 		module[curmodule].released = 0;
@@ -172,21 +184,22 @@ public:
 
 	CALresult AddName(CALname* nam, CALmodule mod)
 	{
+		//printf("Giving name %d (mod %d)\n", curname, mod);
 		pthread_mutex_lock(&mutex);
 		if (curname == NUM_FAKE_NAME)
 		{
 			fprintf(stderr, "NUM_FAKE_NAME overflow\n");
-			exit(1);
+			while(true);
 		}
-		if (mod > curmodule)
+		if (mod > (unsigned) curmodule)
 		{
 			fprintf(stderr, "Invalid Module\n");
-			exit(1);
+			while(true);
 		}
 		if (module[mod].nnames == NUM_MODULE_NAMES)
 		{
 			fprintf(stderr, "NUM_MODULE_NAMES overflow\n");
-			exit(1);
+			while(true);
 		}
 		*nam = curname;
 		module[mod].names[module[mod].nnames] = curname;
@@ -203,7 +216,7 @@ public:
 		if (mem[mem1].active || mem[mem2].active)
 		{
 			fprintf(stderr, "Memory active when starting memcpy\n");
-			exit(1);
+			while(true);
 		}
 		AddEvent(ev, false);
 		event[*ev].nmems = 2;
@@ -218,17 +231,17 @@ public:
 	CALresult FakeKernel(CALfunc func, CALevent* ev)
 	{
 		pthread_mutex_lock(&mutex);
-		if (func > curmodule)
+		if (func > (unsigned) curmodule)
 		{
 			fprintf(stderr, "Invalid func/module");
-			exit(1);
+			while(true);
 		}
 		for (int i = 0;i < module[func].nnames;i++)
 		{
 			if (mem[name[module[func].names[i]].mem].active)
 			{
 				fprintf(stderr, "Memory active when starting kernel\n");
-				exit(1);
+				while(true);
 			}
 			mem[name[module[func].names[i]].mem].active = 1;
 		}
@@ -241,10 +254,10 @@ public:
 
 	CALresult SetMem(CALname nam, CALmem m)
 	{
-		if (nam > curname || m > curmem)
+		if (nam > (unsigned) curname || m > (unsigned) curmem)
 		{
 			fprintf(stderr, "Invalid name/mem\n");
-			exit(1);
+			while(true);
 		}
 		name[nam].mem = m;
 		return(CAL_RESULT_OK);
@@ -271,7 +284,8 @@ public:
 
 cal_fake fake;
 
-#define calCtxRunProgram(event, ctx, func, rect) fake.AddEvent(event)
+#ifndef PASSTHROUGH
+#define calCtxRunProgram(event, ctx, func, rect) fake.FakeKernel(func, event)
 #define calMemCopy(event, ctx, src, dest, flags) fake.FakeMemcpy(src, dest, event)
 #define calCtxIsEventDone(ctx, event) fake.QueryEvent(event)
 #define calCtxGetMem(mem, ctx, res) fake.AddMemHandle(mem)
@@ -281,3 +295,78 @@ cal_fake fake;
 #define calModuleUnload(ctx, module) fake.UnloadModule(module)
 #define calModuleGetName(name, ctx, module, string) fake.AddName(name, module)
 #define calModuleGetEntry(func, ctx, module, string) fake.GetFunc(func, module)
+#else
+
+static inline CALresult calCtxRunProgram_a(CALevent* event, CALcontext ctx, CALfunc func, CALdomain* rect)
+{
+	fake.FakeKernel(func, event);
+	return(calCtxRunProgram(&fake.event[*event].through, ctx, fake.module[func].throughFunc, rect));
+}
+
+static inline CALresult calMemCopy_a(CALevent* event, CALcontext ctx, CALmem src, CALmem dest, CALuint flags)
+{
+	fake.FakeMemcpy(src, dest, event);
+	return(calMemCopy(&fake.event[*event].through, ctx, fake.mem[src].through, fake.mem[dest].through, flags));
+}
+
+static inline CALresult calCtxIsEventDone_a(CALcontext ctx, CALevent event)
+{
+	CALresult retVal = calCtxIsEventDone(ctx, fake.event[event].through);
+	if (retVal == CAL_RESULT_OK) fake.QueryEvent(event);
+	return(retVal);
+}
+
+static inline CALresult calCtxGetMem_a(CALmem* mem, CALcontext ctx, CALresource res)
+{
+	fake.AddMemHandle(mem);
+	return(calCtxGetMem(&fake.mem[*mem].through, ctx, res));
+}
+
+static inline CALresult calCtxSetMem_a(CALcontext ctx, CALname name, CALmem mem)
+{
+	fake.SetMem(name, mem);
+	return(calCtxSetMem(ctx, fake.name[name].through, fake.mem[mem].through));
+}
+
+static inline CALresult calCtxReleaseMem_a(CALcontext ctx, CALmem mem)
+{
+	fake.ReleaseMem(mem);
+	return(calCtxReleaseMem(ctx, fake.mem[mem].through));
+}
+
+static inline CALresult calModuleLoad_a(CALmodule* module, CALcontext ctx, CALimage image)
+{
+	fake.AddModule(module);
+	return(calModuleLoad(&fake.module[*module].through, ctx, image));
+}
+
+static inline CALresult calModuleUnload_a(CALcontext ctx, CALmodule module)
+{
+	fake.UnloadModule(module);
+	return(calModuleUnload(ctx, fake.module[module].through));
+}
+
+static inline CALresult calModuleGetName_a(CALname* name, CALcontext ctx, CALmodule module, const CALchar* symbolname)
+{
+	fake.AddName(name, module);
+	return(calModuleGetName(&fake.name[*name].through, ctx, fake.module[module].through, symbolname));
+}
+
+static inline CALresult calModuleGetEntry_a(CALfunc* func, CALcontext ctx, CALmodule module, const CALchar* symbolname)
+{
+	fake.GetFunc(func, module);
+	return(calModuleGetEntry(&fake.module[module].throughFunc, ctx, fake.module[module].through, symbolname));
+}
+
+#define calCtxRunProgram calCtxRunProgram_a
+#define calMemCopy calMemCopy_a
+#define calCtxIsEventDone calCtxIsEventDone_a
+#define calCtxGetMem calCtxGetMem_a
+#define calCtxSetMem calCtxSetMem_a
+#define calCtxReleaseMem calCtxReleaseMem_a
+#define calModuleLoad calModuleLoad_a
+#define calModuleUnload calModuleUnload_a
+#define calModuleGetName calModuleGetName_a
+#define calModuleGetEntry calModuleGetEntry_a
+
+#endif
